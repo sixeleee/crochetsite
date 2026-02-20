b25db668535f2e6ff6aabd0dd9058f6c0f4db259
Lee Alexis Meru <142468164+sixeleee@users.noreply.github.com>
2026-02-20 21:47:51 +0800

    Update settings.py

--- crochet_site/settings.py
+++ crochet_site/settings.py
@@ -19,9 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a29*5()wei^z7iu0pgth8w-n9ia9l^c)ma3dh&h!tb)5i0!*br'
+SECRET_KEY = 'django-insecure-a29*5()wei^z7iu0pgth8w-n9ia9l^c)ma3dh&hi0!*br'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
